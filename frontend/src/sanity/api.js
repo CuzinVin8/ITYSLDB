183f4acb5a9166e486724d7f934b81a8dd4441ac
CuzinVin <vps108@gmail.com>
2022-12-10 01:12:18 -0500

    default order by season, episode, title

--- frontend/src/sanity/api.js
+++ frontend/src/sanity/api.js
@@ -2,7 +2,7 @@ import {useSanityClient} from 'astro-sanity'
 
 export async function getAllQuotes() {
     const client = useSanityClient()
-    const query = '*[_type == "quote"]'
+    const query = '*[_type == "quote"] | order(season asc, episode asc, sketch asc)'
     const quotes = await client.fetch(query)
     return quotes;
 }
