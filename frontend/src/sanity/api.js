d6a9765aaa51b7175221aa1f3a677031da606eb5
CuzinVin <vps108@gmail.com>
2022-12-09 21:21:22 -0500

    Restored with input field

--- frontend/src/sanity/api.js
+++ frontend/src/sanity/api.js
@@ -6,24 +6,3 @@ export async function getAllQuotes() {
     const quotes = await client.fetch(query)
     return quotes;
 }
-
-export async function getSketches() {
-    const client = useSanityClient()
-    const query = '*[_type == "quote"]'
-    const quotes = await client.fetch(query)
-    return quotes;
-}
-
-export async function getSearchResults(search, sketch) {
-    const client = useSanityClient()
-    const query = `*[quote match '*${search}*']`
-    const quotes = await client.fetch(query)
-    return quotes;
-}
-
-function onlyUnique(value, index, self) {
-    return self.indexOf(value) === index;
-}
-
-//*[quote match "*tuna*"]  //quotes contain the word tuna
-//*[_type == "quote"]   //all quote items
